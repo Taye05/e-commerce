de00f100c0cee0907210e001be06f5a21e48be02
@Taye05 <adebamowotaiwoesther@gmail.com>
2022-06-08 11:45:20 +0100

    cart items

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -8,9 +8,44 @@ import { urlFor } from "../lib/client";
 
 const Cart = () => {
     const cartRef = useRef();
+    const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext()
     return (
-        <div>
-            cart
+        <div className="cart-wrapper" ref={cartRef}>
+            <div className="cart-container">
+                <button 
+                 type="button"
+                 className="cart-heading"
+                 onClick={() => setShowCart(false)}
+                >
+                    <AiOutlineLeft />
+                    <span className="heading">Your Cart</span>
+                    <span className="cart-num-items">({totalQuantities} items)</span>
+                </button>
+
+                {cartItems.length < 1 && (
+                    <div className="empty-cart">
+                        <AiOutlineShopping size={150}/>
+                        <h3>Your shopping bag is empty</h3>
+                        <Link href="./">
+                            <button 
+                              type="button"
+                              onClick={() => setShowCart(false)}
+                              className="btn"
+                            >
+                               Continue Shopping
+                            </button>
+                        </Link>
+                    </div>
+                )}
+
+                <div className="product-container">
+                    {cartItems.length >= 1 && cartItems.map((item) => (
+                        <div className="product" key={item._id}>
+                            <img src={urlFor(item.image[0])}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
